964e662b23bf3c4104f5affb17f1be5893cd171a
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-07-24 02:34:56 +0900

    setup bootstrap in tests/cli

--- tests/cli/src/components/HelloWorld.vue
+++ tests/cli/src/components/HelloWorld.vue
@@ -1,44 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Modal Samples</h1>
+    <bs-modal ref="bodyOnlyModalRef">
+      <h1>Body</h1>
+    </bs-modal>
+    <button type="button" class="btn btn-primary" v-on:click="showModal()">Show Modal</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent,ref } from 'vue';
+import modalComponent from "../../../../src/bs-modal.vue";
 export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: String,
   },
+  setup(){
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const bodyOnlyModalRef = ref<any>(null);
+    return{
+      bodyOnlyModalRef,
+      showModal(){
+        bodyOnlyModalRef.value?.showModal();
+      }
+    }
+  },
+  components:{
+    // eslint-disable-next-line vue/no-unused-components
+    "bs-modal":modalComponent
+  }
 });
 </script>
 
