28b672b48eeb3b2284ce446f608bbfb12a3a5774
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-07-26 03:41:56 +0900

    update tests/cli *add modal option tests

--- tests/cli/src/components/HelloWorld.vue
+++ tests/cli/src/components/HelloWorld.vue
@@ -4,31 +4,65 @@
     <bs-modal ref="bodyOnlyModalRef">
       <h1>Body</h1>
     </bs-modal>
-    <button type="button" class="btn btn-primary" v-on:click="showModal()">Show Modal</button>
+    <bs-modal ref="footerModalRef">
+        <h1>Body</h1>
+      <template v-slot:footer>
+        <h1>Footer</h1>
+      </template>
+    </bs-modal>
+    <modal-in-modal ref="modalInModalRef">
+      <button type="button" class="btn btn-primary m-1" v-on:click="showModal(modalInModalRef)">Show Modal in Modal</button>
+    </modal-in-modal>
+    <button type="button" class="btn btn-primary m-1" v-on:click="showModal(bodyOnlyModalRef)">Show Body Only Modal</button><br>
+    <button type="button" class="btn btn-primary m-1" v-on:click="showModal(footerModalRef)">Show Modal</button><br>
+    <button type="button" class="btn btn-primary m-1" v-on:click="showModal(modalInModalRef)">Show Modal in Modal</button><br>
+    <hr>
+    <h3>.modal Settings</h3>
+    <bs-modal ref="staticBackdropRef" data-bs-backdrop="static">
+      <h1> Static Backdrop</h1>
+    </bs-modal>
+    <button type="button" class="btn btn-primary m-1" v-on:click="showModal(staticBackdropRef)">Static Backdrop</button>
+    <hr>
+    <h3>.modal-dialog Settings</h3>
+    <bs-modal ref="scrollingLongContentRef" :modal-dialog-settings="['modal-dialog-scrollable']">
+      Scrolling Long Content
+      ...<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      end
+    </bs-modal>
+    <bs-modal ref="xlSizeModalRef" :modal-dialog-settings="['modal-xl']"><h1>XL Size!</h1></bs-modal>
+    <bs-modal ref="fullscreenModalRef" :modal-dialog-settings="['modal-fullscreen']"><h1>Fullscreen!</h1></bs-modal>
+    <button type="button" class="btn btn-primary m-1" v-on:click="showModal(scrollingLongContentRef)">Scrolling Long Content</button>
+    <button type="button" class="btn btn-primary m-1" v-on:click="showModal(xlSizeModalRef)">Xl Size</button>
+    <button type="button" class="btn btn-primary m-1" v-on:click="showModal(fullscreenModalRef)">Fullscreen</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent,ref } from 'vue';
-import {bsModal} from "../../../../dist";
+import {BsModal,BsModalInterface} from "../../../../dist";
+import ModalInModal from "./ModalInModal.vue";
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
   setup(){
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const bodyOnlyModalRef = ref<any>(null);
+    const makeRef  = () => ref<any>(null);
     return{
-      bodyOnlyModalRef,
-      showModal(){
-        bodyOnlyModalRef.value?.showModal();
+      bodyOnlyModalRef:makeRef(),
+      footerModalRef:makeRef(),
+      modalInModalRef:makeRef(),
+      staticBackdropRef:makeRef(),
+      scrollingLongContentRef:makeRef(),
+      xlSizeModalRef:makeRef(),
+      fullscreenModalRef:makeRef(),
+      showModal(refValue:BsModalInterface){
+        refValue?.showModal();
       }
     }
   },
   components:{
-    bsModal
-  }
+    BsModal,
+    ModalInModal
+}
 });
 </script>
 
---
+++ tests/cli/src/components/ModalInModal.vue
@@ -0,0 +1,33 @@
+<template>
+    <bs-modal ref="thisRef">
+        <button type="button" class="btn btn-primary mb-1" v-on:click="showModalInModal(modalInModalRef)">Show Modal in Modal</button>
+    </bs-modal>
+    <bs-modal ref="modalInModalRef">
+        <h1> Modal in Modal</h1>
+    </bs-modal>
+</template>
+<script lang="ts">
+import { defineComponent,ref } from 'vue';
+import {BsModal,BsModalInterface} from "../../../../dist";
+export default defineComponent({
+    setup(){
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const makeRef  = () => ref<any>(null);
+        const thisRef = makeRef();
+        return {
+            modalInModalRef:makeRef(),
+            thisRef,
+            showModal(){
+                thisRef.value?.showModal();
+            },
+            showModalInModal(refValue:BsModalInterface){
+                refValue?.showModal();
+            }
+        }
+    },
+    components:{
+        // eslint-disable-next-line vue/no-unused-components
+        BsModal
+    }
+});
+</script>
