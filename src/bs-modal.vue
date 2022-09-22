b6beae93ff194bc719cbc34c5db2f9301fd9a23d
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-09-22 21:25:34 +0900

    remove "fade" class (bs-modal component) https://github.com/s-n-1-0/bs-modal.vue/issues/2

--- src/bs-modal.vue
+++ src/bs-modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="modal fade" tabindex="-1" aria-hidden="true" ref="modal">
+    <div class="modal" tabindex="-1" aria-hidden="true" ref="modal">
         <div class="modal-dialog" :class="modalDialogClass">
             <div class="modal-content">
                 <div class="modal-header">
